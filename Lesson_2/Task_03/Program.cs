8c3837ab1b656881ae0b9adfcc913e5399272d26
Aleksandra Ledeneva <a.ledeneva@bk.ru>
2022-10-24 16:26:31 +0300

    Добавила решение задачи 3 и 4 ДЗ по семинару 2

---
+++ Lesson_2/Task_03/Program.cs
@@ -0,0 +1,33 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет. Не использовать строки для расчета.
+// 645 -> 5
+// 78 -> третьей цифры нет
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine($"В числе {number} нет третьей цифры");
+        return false;
+    }
+    return true;
+}
+int number = Prompt ("Введите число: ");
+int newnumber = number;
+if(ValidateNumber(number))
+{
+    while(newnumber > 1000)
+    {
+        newnumber = newnumber / 10;       
+    }
+    int thirdDigit = newnumber % 10;
+    Console.WriteLine($"Третья цифра числа {number} - {thirdDigit}");
+}
+
---
+++ Lesson_2/Task_04/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int day = Prompt ("Введите цифру, обозначающую день недели: ");
+if (day >= 1 && day <= 5)
+{
+   Console.WriteLine ($"Этот день не является выходным");
+}
+else if (day >=6 && day <= 7)
+{
+    Console.WriteLine ($"Этот день является выходным");
+}
+else
+{
+    Console.WriteLine ($"Такого дня недели не существует");
+}
+
