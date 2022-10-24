4812353ff15bad6a8fd2460cb37d47a57fc98924
Aleksandra Ledeneva <a.ledeneva@bk.ru>
2022-10-24 16:17:46 +0300

    Добавила решение задачи 1

---
+++ Lesson_2/Task_01/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+// 456 -> 5
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int number = Prompt("Введите трехзначное число: ");
+
+bool ValidateNumber(int number)
+{
+    if (number < 100 || number >=1000)
+    {
+        Console.WriteLine("Это число не трехзначное");
+        return false;
+    }
+    return true;
+}
+
+if(ValidateNumber(number))
+{
+    while(number >=100)
+    {
+        number /= 10;
+        int secondDigit = number % 10;
+        Console.WriteLine($"Вторая цифра трехзначного числа - {secondDigit}");
+    }
+}
