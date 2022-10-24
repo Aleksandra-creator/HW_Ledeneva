ae05719055a2eaa17abb02889d194d77bd4e8495
Aleksandra Ledeneva <a.ledeneva@bk.ru>
2022-10-24 16:21:51 +0300

    Добавила решение второй задачи

---
+++ Lesson_2/Task_02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+// 456 -> 46
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число - {number}");
+
+int firstDigit = number % 10;
+int secondDigit = number / 100;
+int result = secondDigit * 10 + firstDigit;
+Console.WriteLine($"После удаления второй цифры получаем число - {result}");
+
