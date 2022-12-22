3dad49eca7cc2b164ce4e906e2a8c7fd3d62aa7f
Aleksandra Ledeneva <a.ledeneva@bk.ru>
2022-12-22 11:53:19 +0300

    Добавлено домашнее задание

---
+++ Lesson_9/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void EvenNumbers (int n, int m)
+{
+    for (int i = n; i <= m; i++)
+        {
+            if (m > n && i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+}
+
+void Result ()
+{
+    int n = 1;
+    int m = 8;
+    Console.WriteLine(EvenNumbers(n, m));
+}
+Result();
+
+// string GetEven(int m, int n) // возвращает строку с четными числами через запятую
+// {
+//     if (m % 2 != 0 && m < n) m += 1; // приводим к четности
+//     if (m < 0) m = 2; // натуральные числа будут начинаться с 2 
+//     if (m > n) return ""; // условие выхода из рекурсии
+//     else return ($"{m}, " + GetEven(m + 2, n)).TrimEnd(' ', ','); 
+// }
+
+// void Main() // тело программы
+// {
+//     int m = -100;
+//     int n = 30;
+//     System.Console.WriteLine(GetEven(m, n));
+// }
+
+// Main();
---
+++ Lesson_Final/Task_02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N 
+// с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int SumOfNumbers(int m, int n) 
+{
+    if (m < 0) m = 1;
+    if (m > n) return 0; 
+    else return (m + SumOfNumbers(m + 1, n));
+}
+
+void Result() 
+{
+    int m = 4;
+    int n = 8;
+    Console.WriteLine(SumOfNumbers(m, n));
+}
+
+Result();
